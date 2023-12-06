67d0268c41cb32c4385127526fdf966988af68ae
Rubén Saavedra <resaavedrav@gmail.com>
2023-12-06 20:44:07 +0000

    Deleted application files from TfL image processing UI

--- TfL image processing UI/run_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "${bearerToken}" > /usr/share/nginx/html/bearer_token
-echo "${Quix__Workspace__Id}" > /usr/share/nginx/html/workspace_id
-echo "${Quix__Portal__Api}" > /usr/share/nginx/html/portal_api
-echo "${Quix__Deployment__Id}" > /usr/share/nginx/html/Quix__Deployment__Id
-echo "${processed}" > /usr/share/nginx/html/processed_topic
-nginx -g "daemon off;"
